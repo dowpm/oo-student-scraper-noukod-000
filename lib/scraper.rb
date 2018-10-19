f2fba0b5e5bb46cd37301b78d44ab5d6fb96fbd6
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-19 16:01:35 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,12 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     srape_url = Nokogiri::HTML(open(index_url))
-    srape_url.css(".student-card")
-    binding.pry
+    arr = []
+    srape_url.css(".student-card a").map do |html_student|
+      {name: html_student.css("h4").text, location: html_student.css("p").text, profile_url: html_student.attribute("href").value}
+    end
+    # binding.pry
+    
   end
 
   def self.scrape_profile_page(profile_url)
@@ -15,5 +19,5 @@ class Scraper
   end
 
 end
-
+binding.pry
 Scraper.scrape_index_page './fixtures/student-site/index.html'
