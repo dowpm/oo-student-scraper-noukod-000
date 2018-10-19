82ce034f299e2e13c8620fdefafbf18427ff6f98
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-19 15:38:49 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    http = nokogiri::HTML(open(index_url))
+    srape_url = nokogiri::HTML(open(index_url))
+    scrape_url.css(".student-card")
+    binding.pry 
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
